b5a07871c9b190112eda3caacee681655cc08822
c1322231 Jonatas Senna de Oliveira <c1322231@interno.bb.com.br>
2022-05-09 19:04:50 -0300

    Finalização do projeto de exemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'jad-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "jad-root",
+  templateUrl: "./app.component.html",
 })
 export class AppComponent {
-  title = 'jad works!';
+  students = [
+    { name: "Luke", isJedi: true, temple: "Coruscant" },
+    { name: "Han Solo", isJedi: false },
+    { name: "Leia", isJedi: false },
+  ];
 }
